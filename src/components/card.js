97a7a50994df7b8edb26ce53066f1ac60a8cbb1b
karthikpratihpati <karthikprathipati1@gmail.com>
2022-05-31 14:38:41 +0530

    added some css and changed the pictures

--- src/components/card.js
+++ src/components/card.js
@@ -2,13 +2,15 @@ export default function Card(prop) {
     return (
         <div className="card">
             <img src={prop.imageUrl} className="image" />
-            <div className="card--location">
-                <p>{prop.location}</p>
-                <p><a href={prop.googleMapsUrl}>View on google maps</a></p>
+            <div className="card--body">
+                <div className="card--location">
+                    <p>{prop.location}</p>
+                    <p className="view"><a href={prop.googleMapsUrl}>View on google maps</a></p>
+                </div>
+                <p className="card--place">{prop.title}</p>
+                <p className="card--date">{prop.startDate} - {prop.endDate}</p>
+                <p className="card--des">{prop.description}</p>
             </div>
-            <p className="card--place">{prop.title}</p>
-            <p className="card--date">{prop.startDate} - {prop.endDate}</p>
-            <p className="card--des">{prop.description}</p>
         </div>
     );
 }
